566c8ba655032e7ae2518e17651bed2d6be60f43
Changhui Xu <changhuixu@hotmail.com>
2020-03-19 08:49:30 -0500

    update to Angular 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NgxQuillLite } from 'projects/ngx-quill-lite/src/public_api';
+import { NgxQuillLite } from 'projects/ngx-quill-lite/src/public-api';
 import { Editor1Component } from './editor1/editor1.component';
 import { Editor2Component } from './editor2/editor2.component';
 import { AppRoutingModule } from './app.routing';
